fd0a17bc1c1ea9729ff67ca6bac554d8f95ca0f9
Anton <anton.solovev.1990@mail.ru>
2023-05-22 16:33:38 +0700

    1

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+export default class ErrorRepository {
+    constructor() {
+      this.errors = new Map();
+      this.errors.set(200, 'Status OK');
+    }
+  
+    translate(code) {
+      if (this.errors.has(code)) {
+        return this.errors.get(code);
+      }
+      return 'Unknown error';
+    }
+  }
